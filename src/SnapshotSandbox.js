f9ec4e3cbb13bffa248d3e5216079ad8eb959dd1
haixiangyan <haixiang6123@gmail.com>
2022-09-27 22:54:53 +0800

    feat: 添加 jest 准备单测

---
+++ src/SnapshotSandbox.js
@@ -0,0 +1,7 @@
+class SnapshotSandbox {
+  constructor() {
+    console.log(123);
+  }
+}
+
+export default SnapshotSandbox;
---
+++ tests/SnapshotSandbox.test.js
@@ -0,0 +1,5 @@
+describe('SnapshotSandbox', () => {
+  it('能运行', () => {
+    expect(1 + 1).toEqual(2);
+  })
+});

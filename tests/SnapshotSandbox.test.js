b5ce60861068fc2149a36f3cd93e0ccf094da918
haixiangyan <haixiang6123@gmail.com>
2022-09-28 09:44:42 +0800

    feat: 添加 SnapshotSandbox 的实现以及测试

--- tests/SnapshotSandbox.test.js
+++ tests/SnapshotSandbox.test.js
@@ -1,5 +1,35 @@
+const SnapshotSandbox = require('../src/SnapshotSandbox');
+
 describe('SnapshotSandbox', () => {
-  it('能运行', () => {
-    expect(1 + 1).toEqual(2);
+  afterEach(() => {
+    delete window.value;
+  })
+
+  it('可以激活环境', () => {
+    const sandbox = new SnapshotSandbox();
+
+    // 假设以前有一个旧值 value: 1
+    sandbox.modifiedMap = {
+      value: 1 // 旧值
+    }
+
+    // 需要恢复 value: 1 -> window
+    sandbox.active();
+
+    expect(window.value).toEqual(1);
+  })
+
+  it('可以在失活时记录环境', () => {
+    const sandbox = new SnapshotSandbox();
+
+    // 准备激活
+    window.value = 321;
+    sandbox.active();
+    expect(sandbox.windowSnapshot.value).toEqual(321);
+
+    // 准备失活
+    window.value = 123;
+    sandbox.inactive();
+    expect(sandbox.modifiedMap.value).toEqual(123);
   })
 });
